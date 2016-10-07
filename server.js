3e5635eb4086b6a5e0d2ffbb221f2dfaf78dedad
Anton Korovaykin <anton.korovaykin@arvosoftware.com>
2016-10-07 18:37:48 +0300

    Backend validation

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.post('/api/ccv2', function(req, res) {
+  var value = req.body.value;
+
+  res.json({
+    ccv2Valid: !isNaN(parseInt(value)) && 100 <= value && value <= 999
+  });
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
